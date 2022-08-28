df618f0655efc201100fa59892cd1ebf73bdfff7
Anna <lancerita01@gmail.com>
2022-08-28 18:47:01 +0300

    Изменение в манифесте (Theme)

--- app/src/main/java/com/example/moviechest/MainActivity.kt
+++ app/src/main/java/com/example/moviechest/MainActivity.kt
@@ -1,50 +1,60 @@
 package com.example.moviechest
 
-import android.content.Intent
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviechest.databinding.ActivityMainBinding
+
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-lateinit var filmName1 : TextView
-lateinit var filmName2 : TextView
-lateinit var filmName3 : TextView
+   // private val recyclerView = findViewById<RecyclerView>(R.id.recycler)
+    //val adapter() = MovieAdapter()
+    private var movies = arrayListOf<MovieItem>(
+        MovieItem(
+            R.string.la_casa_del_papel.toString(),
+            R.drawable.la_casa_del_papel,
+            R.string.buttonItem.toString()
+        ),
+        MovieItem(
+            R.string.razzhimaya_kulaki.toString(),
+            R.drawable.razhimaya_kulaki,
+            R.string.buttonItem.toString()
+        ),
+        MovieItem(
+            R.string.years_and_years.toString(),
+            R.drawable.years_and_years,
+            R.string.buttonItem.toString()
+        )
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initRecycler()
+//      initClickListeners()
 
-
-        setContentView(R.layout.activity_main)
-
-            filmName1 = findViewById<TextView>(R.id.filmName1)
-            val button1 = findViewById<Button>(R.id.button1)
-        button1.setOnClickListener{
-filmName1.setTextColor(Color.parseColor("#FF03DAC5"))
-            startActivity(Intent(this, LaCasaDelPapel::class.java))
-        }
-        filmName2 = findViewById<TextView>(R.id.filmName2)
-        val button2 = findViewById<Button>(R.id.button2)
-        button2.setOnClickListener {
-            filmName2.setTextColor(Color.parseColor("#FF03DAC5"))
-            startActivity(Intent(this, YearsAndYears::class.java))
-        }
-
-        filmName3 = findViewById<TextView>(R.id.filmName3)
-        val button3 = findViewById<Button>(R.id.button3)
-        button3.setOnClickListener {
-            filmName3.setTextColor(Color.parseColor("#FF03DAC5"))
-            startActivity(Intent(this, RazhimayaKulaki::class.java))
-        }
-
-        findViewById<Button>(R.id.buttonInvite).setOnClickListener{
-findViewById<TextView>(R.id.EmailAddress).visibility = View.VISIBLE
-        }
     }
+
+    private fun initRecycler() {
+        binding.apply {
+            recycler?.layoutManager = LinearLayoutManager(this@MainActivity)
+            recycler?.adapter = MovieAdapter(movies)
+        }
+
     }
+
+
+}
+
+
+
+
+
+
 
 
